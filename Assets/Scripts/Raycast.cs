ca1f0dcc2eedb432d6567f624b72aa8019e22bd9
hyunS00 <optifun72@gmail.com>
2018-09-06 00:11:58 +0900

    good

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour {
+    private void Start()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    }
+}
